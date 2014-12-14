81df2b7fe054eb80e856e7a58be9b3f6459cd90c
huangyuxuan01 <huangyuxuan01@live.com>
2014-12-14 17:04:43 +0800

    Fist commit for galaxys2 omap4

---
+++ resources/include/generated/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 13 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#13 SMP PREEMPT Sat Sep 17 21:59:34 CST 2011"
+#define LINUX_COMPILE_TIME "21:59:34"
+#define LINUX_COMPILE_BY "xiaolu"
+#define LINUX_COMPILE_HOST "ubuntu-xiaolu"
+#define LINUX_COMPILE_DOMAIN "(none)"
+#define LINUX_COMPILER "gcc version 4.5.1 (Ubuntu/Linaro 4.5.1-7ubuntu1) "
